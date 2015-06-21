dc4907dbd62ab6ae93adc9958621cd61ce5229f8
unknown <antonio.vianna.pd@gmail.com>
2015-06-21 19:56:19 -0300

    Botoes next e previous

--- js/cinema.js
+++ js/cinema.js
@@ -1,3 +1,4 @@
+
 $('.profile-button').each(function(i,elem){
     var button = $(elem);
     button.css('width', button.attr('width'));
@@ -5,14 +6,6 @@ $('.profile-button').each(function(i,elem){
     button.css('top', button.attr('top'));
     button.css('left', button.attr('left'));
     button.click(function() {
-		var nome = $(this).attr("name");
-		var main = $(this).parent().parent();
-		$.getJSON('profile-data.json', function(data) {
-			$.get('perfil.html', function(template){
-				var templateFunction = doT.template(template);
-				var html = templateFunction(data[nome]);
-				main.html(html);
-			});
-		});
+		Profile.loadProfile($(this).attr("name"), $(this).parent().parent());
     });
 });
--- js/post.js
+++ js/post.js
@@ -5,3 +5,14 @@ $('[data-target]').click( function (e) {
     $('#banner').addClass('invisible');
     e.preventDefault(); // prevent anchor from changing window.location
 });
+
+Profile = {};
+Profile.loadProfile = function(nome, main) {
+		$.getJSON('profile-data.json', function(data) {
+			$.get('perfil.html', function(template){
+				var templateFunction = doT.template(template);
+				var html = templateFunction(data[nome]);
+				main.html(html);
+			});
+		});
+}
