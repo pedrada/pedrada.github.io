deaf68f60e778b5a58c53b7cae9cbfc52973979a
pedrada <antonio.vianna.pd@gmail.com>
2015-08-20 13:43:37 -0300

    Socine start

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 /*
-	Imagination by TEMPLATED
-    templated.co @templatedco
-    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Synchronous by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
 skel.init({
